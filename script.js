af43bb131894c065820960021d560357384a4723
DevVitorPedra <75690532+DevVitorPedra@users.noreply.github.com>
2021-09-16 20:45:36 -0300

    resetStorage

--- script.js
+++ script.js
@@ -56,12 +56,12 @@ const salvarLista = () => {
     itemArray.push(itemObj);
     itemObj = {};
   }
-  if(itemArray!=''){
-  toStorage();
-  itemArray = [];
-  console.log(itemArray);
-  }else{
-    alert('lista vazia')
+  if (itemArray != "") {
+    toStorage();
+    itemArray = [];
+    console.log(itemArray);
+  } else {
+    alert("lista vazia");
   }
 };
 btnSalvar.addEventListener("click", salvarLista);
@@ -71,8 +71,8 @@ const toStorage = () => {
   localStorage.setItem(`lista ${id}`, JSON.stringify(itemArray));
   adicionados.innerHTML = "";
   console.log("addedeu");
-  sideList.innerHTML=''
-  fromStorage()
+  sideList.innerHTML = "";
+  fromStorage();
 };
 const deleteItem = (ele) => {
   let del = ele.parentElement.getAttribute("name");
@@ -80,74 +80,87 @@ const deleteItem = (ele) => {
   ele.parentElement.remove();
 };
 const deleteFromStorage = (ele) => {
-     let listPosition = ele.previousSibling.innerHTML
-    localStorage.removeItem(listPosition)
-    sideList.innerHTML = ''
-    fromStorage()
-}
-const fromStorage = () => {
-  let storageKeys =[]
-  for(let j = 0; j<localStorage.length; j++){
-    storageKeys.push(localStorage.key(j))
-
-  }
-  storageKeys.sort()
+  let listPosition = ele.previousSibling.innerHTML;
+  localStorage.removeItem(listPosition);
+  sideList.innerHTML = "";
+  resetStorage()
   
+  
+};
+const fromStorage = () => {
+  let storageKeys = [];
+  for (let j = 0; j < localStorage.length; j++) {
+    storageKeys.push(localStorage.key(j));
+  }
+  storageKeys.sort();
+
   for (let i = 0; i < localStorage.length; i++) {
-  let key = storageKeys[i]
+    let key = storageKeys[i];
     let newItem = document.createElement("div");
     let newItemName = document.createTextNode(key);
-    let itemDelete = document.createElement('button')
-    itemDelete.setAttribute('onclick', 'deleteFromStorage(this)')
+    let itemDelete = document.createElement("button");
+    itemDelete.setAttribute("onclick", "deleteFromStorage(this)");
 
-    itemDelete.innerHTML ='<i class="bi bi-trash"></i>'
-    itemDelete.classList.add('delete-storage')
+    itemDelete.innerHTML = '<i class="bi bi-trash"></i>';
+    itemDelete.classList.add("delete-storage");
     newItem.appendChild(newItemName);
-    newItem.setAttribute('onclick','testingSideList(this)')
+    newItem.setAttribute("onclick", "testingSideList(this)");
     newItem.classList.add("card");
-    
-    
-    sideList.appendChild(newItem);
-  sideList.appendChild(itemDelete)
 
+    sideList.appendChild(newItem);
+    sideList.appendChild(itemDelete);
   }
 };
 fromStorage();
 
-
 const testingSideList = (ele) => {
-  
   adicionados.innerHTML = "";
-  let arrayItem = JSON.parse(localStorage.getItem(ele.innerHTML))
- arrayItem.forEach(lista => {
-  let itemDiv = document.createElement("div");
-  let itemCheckbox = document.createElement("input");
-  itemCheckbox.classList.add("checkboxes");
-  itemCheckbox.setAttribute("type", "checkbox");
-  itemCheckbox.checked = lista.check
-  itemCheckbox.setAttribute("name",lista.name);
-  itemCheckbox.setAttribute("id", lista.name+id);
-  let itemLi = document.createElement("label");
-  itemLi.classList.add("line-through");
-  itemLi.setAttribute("for", lista.name+id);
-  let itemLiNode = document.createTextNode(lista.name);
-  itemLi.appendChild(itemLiNode);
-  let itemSpan = document.createElement("span");
-  itemSpan.innerHTML = '<i class="bi bi-trash"></i>';
-  itemSpan.classList.add("delete");
-  itemSpan.setAttribute("onclick", "deleteItem(this)");
-  itemDiv.appendChild(itemCheckbox);
-  itemDiv.appendChild(itemLi);
-  itemDiv.appendChild(itemSpan);
-  itemDiv.setAttribute("name", lista.name);
+  let arrayItem = JSON.parse(localStorage.getItem(ele.innerHTML));
+  arrayItem.forEach((lista) => {
+    let itemDiv = document.createElement("div");
+    let itemCheckbox = document.createElement("input");
+    itemCheckbox.classList.add("checkboxes");
+    itemCheckbox.setAttribute("type", "checkbox");
+    itemCheckbox.checked = lista.check;
+    itemCheckbox.setAttribute("name", lista.name);
+    itemCheckbox.setAttribute("id", lista.name + id);
+    let itemLi = document.createElement("label");
+    itemLi.classList.add("line-through");
+    itemLi.setAttribute("for", lista.name + id);
+    let itemLiNode = document.createTextNode(lista.name);
+    itemLi.appendChild(itemLiNode);
+    let itemSpan = document.createElement("span");
+    itemSpan.innerHTML = '<i class="bi bi-trash"></i>';
+    itemSpan.classList.add("delete");
+    itemSpan.setAttribute("onclick", "deleteItem(this)");
+    itemDiv.appendChild(itemCheckbox);
+    itemDiv.appendChild(itemLi);
+    itemDiv.appendChild(itemSpan);
+    itemDiv.setAttribute("name", lista.name);
 
-  adicionados.appendChild(itemDiv);
-   
- });
-   
-   
-    }
+    adicionados.appendChild(itemDiv);
+  });
+};
 
+
+const resetStorage = () => {
+  let storageArray  = []     
+       for(let i =0; i<localStorage.length;i++){
+        let key = localStorage.key(i)
+        let refresh = JSON.parse(localStorage.getItem(key))
+        
+        storageArray.push(refresh)
+        
+       console.log(refresh)
+       }
+       localStorage.clear()
+       for(let j = 0;j<storageArray.length; j++){
+         console.log()
+        localStorage.setItem(`lista ${j+1}`,JSON.stringify(storageArray[j]))
+
+       }
+fromStorage()
+}
 
 /**for (let i = 1; i < id; i++) {
     let list = JSON.parse(localStorage.getItem(`lista ${i}`));
